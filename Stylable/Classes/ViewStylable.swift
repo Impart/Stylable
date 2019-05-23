fcb6b6a659a0fa31898e7f12958bbd3c01e4c00e
Marcel Borsten <marcel@impart.nl>
2019-05-23 12:57:46 +0200

    Fixed initializers

--- Stylable/Classes/ViewStylable.swift
+++ Stylable/Classes/ViewStylable.swift
@@ -2,29 +2,64 @@
 //  ViewStylable.swift
 //  layoutTests
 //
-//  Created by Marcel Borsten on 22/05/2019.
+//  Created by Impart IT on 22/05/2019.
 //  Copyright © 2019 Impart. All rights reserved.
 //
 
 import UIKit
 
 public struct BorderStyle {
+
     let cornerRadius: CGFloat?
     let width: CGFloat?
     let color: UIColor?
+
+    public init(cornerRadius: CGFloat? = nil,
+                width: CGFloat? = nil,
+                color: UIColor? = nil) {
+
+        self.cornerRadius = cornerRadius
+        self.width = width
+        self.color = color
+
+    }
 }
 
 public struct ShadowStyle {
+
     let radius: CGFloat?
     let color: UIColor?
     let offset: CGSize?
     let opacity: CGFloat?
+
+    public init(radius: CGFloat? = nil,
+                color: UIColor? = nil,
+                offset: CGSize? = nil,
+                opacity: CGFloat? = nil) {
+
+        self.radius = radius
+        self.color = color
+        self.offset = offset
+        self.opacity = opacity
+
+    }
 }
 
 public struct ViewStyle {
+
     let backgroundColor: UIColor?
     let border: BorderStyle?
     let shadow: ShadowStyle?
+
+    public init(backgroundColor: UIColor? = nil,
+                border: BorderStyle? = nil,
+                shadow: ShadowStyle? = nil) {
+
+        self.backgroundColor = backgroundColor
+        self.border = border
+        self.shadow = shadow
+
+    }
 }
 
 public protocol ViewStylable: class {
